c551f1e7371e431606792d071321cdf977fcf180
Shravastee Thakur <shravastee@gmail.com>
2026-09-24 11:08:47 +0530

    Project completed

--- tools.py
+++ tools.py
@@ -33,13 +33,10 @@ def scrape_url(url: str) -> str:
     Scrape and return clean text content from a given URL for deeper reading.
     """
     try:
-        resp = requests.get(url, timeout=8, headers={"User-Agent", "Mozilla/5.0"})
+        resp = requests.get(url, timeout=8, headers={"User-Agent": "Mozilla/5.0"})
         soup = BeautifulSoup(resp.text, "html.parser")
         for tag in soup(["script", "stype", "nav", "footer"]):
             tag.decompose()
         return soup.get_text(separator=" ", strip=True)[:3000]
     except Exception as e:
         return f"Could not scrape URL: {str(e)}"
-
-
-# print(scrape_url.invoke(""))
